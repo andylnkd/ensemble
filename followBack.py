7abdac02e9df01612696eec79bf0876a0314ba51
m3llad0 <dmelladooliveros@gmail.com>
2022-01-13 20:33:04 -0600

    followback new followers

---
+++ followBack.py
@@ -0,0 +1,13 @@
+from auth import api
+import tweepy
+
+def main():
+    follow()
+
+def follow():
+    #Get account followers
+    for follower in tweepy.Cursor(api.get_followers).items():
+        follower.follow()
+        print('New follow' + follower.screen_name)
+        
+main()
